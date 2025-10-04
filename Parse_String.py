75fea86fbcbb5b3003848cb6b46f7dd7c531707d
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-04 22:20:01 +0530

    Add files via upload

---
+++ Parse_String.py
@@ -0,0 +1,23 @@
+# Parse string into integer
+
+string="12345"
+print(type(string))
+
+int_str=int(string)
+print(type(int_str))
+
+#Parse string into float
+
+string="123.45"
+print(type(string))
+
+float_string=float(string)
+print(type(float_string))
+
+# Parse String Float Numeral into Integer
+
+string="123.45"
+print(type(string))
+str_float_int=int(float(string))
+print(type(str_float_int))
+print(str_float_int)
