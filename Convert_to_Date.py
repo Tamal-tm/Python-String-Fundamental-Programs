04b23a3ef0968e6dbf7576e2ad80aa7a25b6f8da
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-18 08:13:31 +0530

    Update Convert_to_Date.py

--- Convert_to_Date.py
+++ Convert_to_Date.py
@@ -16,7 +16,6 @@ print("Type of datetime_object:", type(datetime_object))
 
 
 import datetime
-
 now = datetime.datetime.now()
 print(now)
 
@@ -33,4 +32,5 @@ date_time=parser.parse("Oct 13 1997 7:15 AM")
 print(date_time)
 
 print(type(date_time))
+
 
