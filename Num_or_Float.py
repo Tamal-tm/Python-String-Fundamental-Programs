e6c9adf8499e6e6c2dbe475e27c7b0da8b14a048
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-31 07:48:21 +0530

    Update Num_or_Float.py

--- Num_or_Float.py
+++ Num_or_Float.py
@@ -3,14 +3,14 @@ num=input("Enter something here: ")
 def float_check(num):
     try:
         float(num)
-        return True
-        
+        return True      
     except:
         return False
         
     
 
 print(float_check(num))
+
 
 
 
